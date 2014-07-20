04f6a30e42aea1e8c61a9a3b9035a778fed31e73
KubaK <voxpol@outlook.com>
2014-07-20 16:51:39 +0200

    a
    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -232,7 +232,7 @@ var theaters = L.geoJson(null, {
   },
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADDRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>" + "<div class='directorySlider' id='slider-main'></div>";
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Místo</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Popis</th><td>" + feature.properties.POPIS + "</td></tr>" + "<tr><th>Doporučujeme</th><td>" + feature.properties.DOPORUCENI + "</td></tr>" + "<tr><th>Sociální zařízení</th><td>" + feature.properties.SOCIALNI + "</td></tr>" + "<tr><th>Občerstvení</th><td>" + feature.properties.OBCERSTVENI + "</td></tr>" + "<tr><th>Doprava</th><td>" + feature.properties.DOPRAVA + "</td></tr>" + "<table>";
       layer.on({
         click: function (e) {
           $("#feature-title").html(feature.properties.NAME);
