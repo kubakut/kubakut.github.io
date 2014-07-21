07f1a5797628932400d5880a7ec3393524b4ce36
KubaK <voxpol@outlook.com>
2014-07-21 16:22:56 +0200

    a
    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -223,7 +223,7 @@ var theaters = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, {
       icon: L.divIcon({
-        html: "<span class='label label-danger'>"+feature.id+"</span>" ,
+        html: "<span class='label label-danger'><h4>"+feature.id+"</h4></span>" ,
         iconSize: [26, 30],
         iconAnchor: [12, 28],
         className: 'label label-danger',        
